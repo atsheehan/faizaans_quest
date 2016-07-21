3c80c6074ea9e78a341dbf9c0c5c730be13311df
Adam Sheehan <a.t.sheehan@gmail.com>
2016-07-21 09:19:41 -0400

    Initial commit
    New Phoenix 1.2.0 app.
    
    ```
    $ mix phoenix.new hookah
    ```

---
+++ lib/hookah.ex
@@ -0,0 +1,31 @@
+defmodule Hookah do
+  use Application
+
+  # See http://elixir-lang.org/docs/stable/elixir/Application.html
+  # for more information on OTP Applications
+  def start(_type, _args) do
+    import Supervisor.Spec
+
+    # Define workers and child supervisors to be supervised
+    children = [
+      # Start the Ecto repository
+      supervisor(Hookah.Repo, []),
+      # Start the endpoint when the application starts
+      supervisor(Hookah.Endpoint, []),
+      # Start your own worker by calling: Hookah.Worker.start_link(arg1, arg2, arg3)
+      # worker(Hookah.Worker, [arg1, arg2, arg3]),
+    ]
+
+    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Hookah.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  # Tell Phoenix to update the endpoint configuration
+  # whenever the application is updated.
+  def config_change(changed, _new, removed) do
+    Hookah.Endpoint.config_change(changed, removed)
+    :ok
+  end
+end
