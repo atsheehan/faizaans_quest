d14da1509067999e4f254069c9f47a6e3052ad51
Adam Sheehan <a.t.sheehan@gmail.com>
2016-08-17 18:29:31 -0400

    Log redux state changes

--- web/static/js/initialize.jsx
+++ web/static/js/initialize.jsx
@@ -5,8 +5,28 @@ import { createStore } from 'redux';
 import Root from './components/Root';
 import reducer from './reducers';
 
+const addLoggingToDispatch = (store) => {
+  const rawDispatch = store.dispatch;
+  if (!console.group) {
+    return rawDispatch;
+  }
+
+  return (action) => {
+    console.group(action.type);
+    console.log('%c prev state', 'color: gray', store.getState());
+    console.log('%c action', 'color: blue', action);
+    const returnValue = rawDispatch(action);
+    console.log('%c next state', 'color: green', store.getState());
+    console.groupEnd(action.type);
+  }
+};
+
 export default (element) => {
   let store = createStore(reducer);
+
+  if (process.env.NODE_ENV !== 'production') {
+    store.dispatch = addLoggingToDispatch(store);
+  }
 
   render(
     <Root store={store} />,
